d9cd09f87fae4c1b730977ec57cacc278a11cb4f
AlexanderMeisinger <alexander.meisinger@oth-regensburg.de>
2025-07-01 05:10:58 -0700

    Add: run multiple scenarios

---
+++ run_industry_scenarios.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+scenarios=(
+    "config.industry_gdp-0.02" 
+    "config.industry_gdp+0" 
+    "config.industry_gdp+0.02"
+    )
+
+for scenario in "${scenarios[@]}"; do
+    path="/mnt/e/H2GMA/Github/AP10/analyse-h2g-a-ap10/config/base-EU-climate-goals/${scenario}.yaml"
+    echo "Running with $path"
+    snakemake --cores all --configfile "$path"
+done
